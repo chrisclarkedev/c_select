f077c1280841f6614d6efc2d618371feada4664b
chrisclarkedev <cclarke8904@gmail.com>
2022-05-19 18:16:41 -0400

    added resized background image

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <header className="App-header">{/* <h1>Hello World</h1> */}</header>
     </div>
   );
 }
