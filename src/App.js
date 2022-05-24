af5e4072f9780a94552014e024fc571f14a07225
chrisclarkedev <cclarke8904@gmail.com>
2022-05-24 18:38:40 -0400

    ussr button styled and positioned with css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import stage4 from './components/sounds/usa.wav';
 import stage5 from './components/sounds/india.wav';
 import stage6 from './components/sounds/japan.wav';
 import stage7 from './components/sounds/usa.wav';
+import stage8 from './components/sounds/brazil.wav';
 
 function App() {
   //Audio for Body Section Of Page (STAGE LOCATIONS) left -> right
@@ -17,7 +18,8 @@ function App() {
   const audio4 = new Audio(stage4);
   const audio5 = new Audio(stage5);
   const audio6 = new Audio(stage6);
-  const audio7 = new Audio(stge7);
+  const audio7 = new Audio(stage7);
+  const audio8 = new Audio(stage8);
 
   const ussr = () => {
     audio.play();
@@ -43,6 +45,14 @@ function App() {
     audio6.play();
   };
 
+  const usa2 = () => {
+    audio7.play();
+  };
+
+  const brazil = () => {
+    audio8.play();
+  };
+
   return (
     <div className="App">
       <header className="App-header">{/* <button>Click ME!</button> */}</header>
@@ -52,17 +62,23 @@ function App() {
         </audio> */}
         {/* FIRST ROW */}
         <div className="firstrow">
-          <button onClick={ussr}>USSR</button>
-          <button onClick={china}>China</button>
+          <div className="btn1">
+            <button onClick={ussr}>USSR</button>
+          </div>
+          <div className="btn2">
+            <button onClick={china}>China</button>
+          </div>
           <button onClick={japan}>Japan</button>
-          <button onClick={usa}>USA</button>
         </div>
 
+        {/* <button onClick={usa}>USA</button> */}
         {/* 2ND ROW */}
-        <div className="secondrow">
+        {/* <div className="secondrow">
           <button onClick={india}>India</button>
           <button onClick={japan2}>Japan2</button>
         </div>
+        <button onClick={usa2}>USA2</button>
+        <button onClick={brazil}>Brazil</button> */}
       </body>
       <footer></footer>
     </div>
